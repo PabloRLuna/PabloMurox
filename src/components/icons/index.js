c0882f8bd20cc9dbe0e4f87fcf1ed3f115a03dad
PabloRLuna <pabloluna.tyko@gmail.com>
2025-06-19 22:24:42 +0200

    Updates

--- src/components/icons/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {
-  Twitter as TwitterIcon,
-  Instagram as InstagramIcon,
-  YouTube as YouTubeIcon,
-  Twitch as TwitchIcon
-} from '@mui/icons-material';
-
-export { TwitterIcon, InstagramIcon, YouTubeIcon, TwitchIcon };
