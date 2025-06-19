6dc8e1b95222d49a46cdc2d086d8c023cfef159a
PabloRLuna <pabloluna.tyko@gmail.com>
2025-06-19 21:38:31 +0200

    Update CSP and proxy configuration

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,26 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
+  // Proxy para Twitch
   app.use(
     '/twitch',
     createProxyMiddleware({
       target: 'https://www.twitch.tv',
+      changeOrigin: true,
+      secure: false,
+      onProxyRes: function(proxyRes, req, res) {
+        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+        proxyRes.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS';
+        proxyRes.headers['Access-Control-Allow-Headers'] = 'Content-Type';
+      }
+    })
+  );
+
+  // Proxy para YouTube
+  app.use(
+    '/api/youtube',
+    createProxyMiddleware({
+      target: 'http://localhost:5000',
       changeOrigin: true,
       secure: false,
       onProxyRes: function(proxyRes, req, res) {
